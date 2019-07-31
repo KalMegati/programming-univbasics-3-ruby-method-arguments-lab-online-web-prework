a00dd51a99feee36a8dfa18b62790a05ba9a13d6
Srikalyan Megati <kalmegati@gmail.com>
2019-07-31 16:31:37 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -6,13 +6,10 @@ introduction("Josh")
 
 introduction("Dan")
 
-def introduction_with_language_optional(name,language)
-  if language
-    puts "Hi, my name is #{name} and I am learning to program in Ruby."
-  else
-    puts "Hi, my name is #{name} and I am learning to program in #{language}."
+def introduction_with_language_optional(name,language = "Ruby")
+  puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end
 
 introduction_with_language_optional("Edwin", "Python")
 
-introduction_with_language_optional("Edwin", "Ruby")
+introduction_with_language_optional("Edwin")
